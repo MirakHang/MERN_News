2c0cb7e40d506f906003ee511dbf85ef1c468c85
MirakLimbu <limboomirak1212@gmail.com>
2024-08-03 00:28:59 +0545

    connected database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.Database_URL)
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const app = express();
 
 app.listen(3000, () => {
