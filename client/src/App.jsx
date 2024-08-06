f71cae0bfd4b949168ca912b3b938605dcc6a992
MirakLimbu <limboomirak1212@gmail.com>
2024-08-06 16:29:47 +0545

    made dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Projects from "./pages/Projects";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Dashboard from "./pages/Dashboard";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
@@ -19,7 +20,9 @@ export default function App() {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
       <Footer />
     </BrowserRouter>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -20,7 +20,7 @@ export default function Header() {
 
   return (
     <div>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900 pb-2">
+      <nav className="bg-white border-gray-700 dark:bg-gray-800 pb-2 border-b-2 ">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto pt-2 px-2">
           <Link
             to="/"
@@ -112,7 +112,7 @@ export default function Header() {
             className="items-center relative w-full justify-between hidden  md:flex md:w-auto md:order-1"
             id="navbar-search"
           >
-            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-800 dark:border-gray-700">
               {navItems.map((item, idx) => (
                 <li key={idx}>
                   <Link
