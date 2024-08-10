d4d75741d9eb0a88c0aaf8109fa542dc9d5d4b1a
MirakLimbu <limboomirak1212@gmail.com>
2024-08-11 02:18:10 +0545

    upload post image functionality

---
+++ api/models/post.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      default:
+        "https://static.vecteezy.com/system/resources/thumbnails/019/167/034/small_2x/cricket-bat-with-wicket-free-png.png",
+    },
+    category: {
+      type: String,
+      default: "uncategorized",
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
